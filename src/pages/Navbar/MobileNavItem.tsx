84255d52b02686cb9aac5c76dbbc4e287a6e6fdb
Dean Trkulja <deantrkulja2001@gmail.com>
2023-03-10 21:35:08 +0100

    Sign out done

--- src/pages/Navbar/MobileNavItem.tsx
+++ src/pages/Navbar/MobileNavItem.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { navItem } from "../../@types/NavbarType";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { routes } from "../../api/paths";
 
 type MobileNavItemType = {
   navItem: navItem;
 };
 export default function MobileNavItem({ navItem }: MobileNavItemType) {
+  const navigate = useNavigate();
+  const signOut = () => {
+    if (navItem.name !== "Sign out") return;
+
+    localStorage.removeItem("staminaUser");
+    navigate(routes.home);
+  };
   return (
     <li>
       <Link
         to={navItem.link}
         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+        onClick={signOut}
       >
         {navItem.name}
       </Link>
