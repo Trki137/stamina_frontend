47e3c4663fe01322d837addc27a813f59b69cfdb
Dean Trkulja <deantrkulja2001@gmail.com>
2023-03-18 12:24:42 +0100

    Filter working

--- src/pages/SearchSidebar/SearchSidebar.tsx
+++ src/pages/SearchSidebar/SearchSidebar.tsx
@@ -13,42 +13,51 @@ type SearchSidebarType = {
   searchActive: boolean;
 };
 export default function SearchSidebar({
-  handleSearchActiveChange,
-  searchActive,
-}: SearchSidebarType) {
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const [users, setUsers] = useState<SearchBarUser[]>([]);
+                                          handleSearchActiveChange,
+                                          searchActive,
+                                      }: SearchSidebarType) {
+    const [searchTerm, setSearchTerm] = useState<string>("");
+    const [users, setUsers] = useState<SearchBarUser[]>([]);
+    const [filterUsers, setFilterUsers] = useState<SearchBarUser[]>([]);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value);
-  };
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(e.target.value);
+    };
 
-  useEffect(() => {
-    let currentUserId = localStorage.getItem("staminaUser");
-    console.log(currentUserId);
-    if (currentUserId == null) return;
-    currentUserId = JSON.parse(currentUserId).userid;
-    console.log(currentUserId);
-    axios
-      .get(`${backend_paths.USERS_URL}/${currentUserId}`)
-      .then((res) => res.data)
-      .then((data: SearchBarUser[]) => setUsers(data))
-      .catch((err) => console.log(err));
-  }, []);
 
-  return (
-    <Sidebar sidebarActive={searchActive}>
-      <FontAwesomeIcon
-        icon={faX}
-        className="ml-2 mt-2 w-3 h-3 cursor-pointer"
-        onClick={handleSearchActiveChange}
-      />
-      <SearchInput value={searchTerm} handleChange={handleChange} />
-      <div className="mt-7 border-t-[1px] border-gray-300">
-        {users.map((user: SearchBarUser) => (
-          <UserProfileBar key={user.userid} user={user} />
-        ))}
-      </div>
-    </Sidebar>
-  );
+    useEffect(() => {
+        let currentUserId = localStorage.getItem("staminaUser");
+        console.log(currentUserId);
+        if (currentUserId == null) return;
+        currentUserId = JSON.parse(currentUserId).userid;
+        console.log(currentUserId);
+        axios
+            .get(`${backend_paths.USERS_URL}/${currentUserId}`)
+            .then((res) => res.data)
+            .then((data: SearchBarUser[]) => {
+                setUsers(data);
+                setFilterUsers(data);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
+    useEffect(() => {
+        setFilterUsers(users.filter(user => user.username.includes(searchTerm)));
+    }, [searchTerm]);
+
+    return (
+        <Sidebar sidebarActive={searchActive}>
+            <FontAwesomeIcon
+                icon={faX}
+                className="ml-2 mt-2 w-3 h-3 cursor-pointer"
+                onClick={handleSearchActiveChange}
+            />
+            <SearchInput value={searchTerm} handleChange={handleChange}/>
+            <div className="mt-7 border-t-[1px] border-gray-300">
+                {filterUsers.map((user: SearchBarUser) => (
+                    <UserProfileBar key={user.userid} user={user}/>
+                ))}
+            </div>
+        </Sidebar>
+    );
 }
