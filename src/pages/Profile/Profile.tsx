d992c36843e217a2c3f26aa332fd382dc0b6c78d
Dean Trkulja <deantrkulja2001@gmail.com>
2023-03-18 17:36:24 +0100

    Profile tab styled and implemented

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ProfileHead from "./ProfileHead";
+import ProfileTab from "../../components/Button/ProfileTab";
 
 export default function Profile() {
+  const [tabIndexActive, setTabIndexActive] = useState<number>(0);
   return (
     <div className="w-full">
-      <ProfileHead />
+      <div className="w-10/12 m-auto">
+        <ProfileHead />
+        <ProfileTab
+          setTabIndexActive={setTabIndexActive}
+          tabIndexActive={tabIndexActive}
+        />
+      </div>
     </div>
   );
 }
