5016414a362a89b1aaf5fe6c3b292b71c270f11c
Dean Trkulja <deantrkulja2001@gmail.com>
2023-03-18 17:12:24 +0100

    Profile head styled

---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProfileHead from "./ProfileHead";
+
+export default function Profile() {
+  return (
+    <div className="w-full">
+      <ProfileHead />
+    </div>
+  );
+}
