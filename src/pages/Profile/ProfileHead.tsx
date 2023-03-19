fc9d5dc76a7ea298c4ef887c250e306db880db36
Dean Trkulja <deantrkulja2001@gmail.com>
2023-03-19 11:19:42 +0100

    Implmented to show diffrent buttons and actions based on profile that user is watching

--- src/pages/Profile/ProfileHead.tsx
+++ src/pages/Profile/ProfileHead.tsx
@@ -7,6 +7,7 @@ import { backend_paths } from "../../api/backend_paths";
 import ModalList from "./ModalList";
 import { FollowerOrFollowing } from "../../@types/UserType";
 import { useParams } from "react-router-dom";
+import Button from "../../components/Button/Button";
 
 type ProfileInfo = {
   username: string;
@@ -16,23 +17,78 @@ type ProfileInfo = {
   name: string;
   numoffollowers: string;
   numoffollowing: string;
+  isfollowing: boolean;
 };
 export default function ProfileHead() {
   const { id } = useParams();
-  const [profileInfo, setProfileInfo] = useState<ProfileInfo>();
+  const [currentUserId, setCurrentUserId] = useState<number>(0);
+  const [profileInfo, setProfileInfo] = useState<ProfileInfo>({
+    username: "",
+    email: "",
+    description: null,
+    image: null,
+    name: "",
+    numoffollowers: "",
+    numoffollowing: "",
+    isfollowing: false,
+  });
   const [followingUser, setFollowingUser] = useState<FollowerOrFollowing[]>([]);
   const [followersUser, setFollowersUser] = useState<FollowerOrFollowing[]>([]);
   const [modalActive, setModalActive] = useState<number>(0);
+
+  const handleUnfollowFollow = (actionId: number) => {
+    if (id === undefined || currentUserId === 0 || profileInfo === undefined)
+      return;
+
+    const data = {
+      followed: id,
+      followedBy: currentUserId,
+    };
+    let path;
+    if (actionId === 1) path = `${backend_paths.USERS_URL}/unfollow`;
+    else path = `${backend_paths.USERS_URL}/follow`;
+
+    console.log(path);
+    axios
+      .post(path, data, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      })
+      .then((res) =>
+        setProfileInfo((prevProfileInfo) => {
+          let newProfileInfo = { ...prevProfileInfo };
+          newProfileInfo.isfollowing = !newProfileInfo.isfollowing;
+          if (actionId === 1)
+            newProfileInfo.numoffollowers = `${
+              parseInt(newProfileInfo.numoffollowers) - 1
+            }`;
+          else
+            newProfileInfo.numoffollowers = `${
+              parseInt(newProfileInfo.numoffollowers) + 1
+            }`;
+          return newProfileInfo;
+        })
+      )
+      .catch((err) => console.log(err));
+  };
 
   useEffect(() => {
     let currentUserId = localStorage.getItem("staminaUser");
     if (currentUserId == null) return;
     currentUserId = JSON.parse(currentUserId).userid;
+    setCurrentUserId(currentUserId === null ? 0 : parseInt(currentUserId));
 
-    console.log(id);
+    const body = {
+      id: currentUserId,
+    };
 
     axios
-      .get(`${backend_paths.USERS_URL}/${id}`)
+      .post(`${backend_paths.USERS_URL}/${id}`, body, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      })
       .then((res) => res.data)
       .then((data) => setProfileInfo(data))
       .catch((err) => console.log(err));
@@ -50,7 +106,6 @@ export default function ProfileHead() {
       .catch((err) => console.log(err));
   }, [id]);
 
-  console.log(profileInfo);
   return (
     <React.Fragment>
       {modalActive > 0 && (
@@ -80,7 +135,25 @@ export default function ProfileHead() {
           <div className="flex max-w-1/2 flex-col gap-y-4 justify-start sm:w-fit">
             <div className="flex flex-col max-w-[250px] w-full sm:flex-row  sm:items-center sm:justify-start sm:gap-x-8">
               <p className="text-2xl font-bold ">{profileInfo?.username}</p>
-              <ProfileButton text="Edit profile" />
+              {id !== undefined && parseInt(id) === currentUserId && (
+                <ProfileButton text="Edit profile" />
+              )}
+              {id !== undefined &&
+                parseInt(id) !== currentUserId &&
+                profileInfo?.isfollowing && (
+                  <Button
+                    text="Unfollow"
+                    handleClick={() => handleUnfollowFollow(1)}
+                  />
+                )}
+              {id !== undefined &&
+                parseInt(id) !== currentUserId &&
+                !profileInfo?.isfollowing && (
+                  <Button
+                    text="Follow"
+                    handleClick={() => handleUnfollowFollow(0)}
+                  />
+                )}
             </div>
             <div className="hidden sm:flex sm:items-center sm:justify-start sm:gap-x-8">
               <p className="cursor-pointer" onClick={() => setModalActive(1)}>
