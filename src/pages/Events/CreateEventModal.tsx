64788a7695fa04a2b4e71694bd8b934a9cfe12ce
Dean Trkulja <deantrkulja2001@gmail.com>
2023-04-30 11:27:46 +0200

    Map implemented

--- src/pages/Events/CreateEventModal.tsx
+++ src/pages/Events/CreateEventModal.tsx
@@ -1,13 +1,33 @@
-import React, { Dispatch, SetStateAction } from "react";
-import Modal from "../../components/Modal/Modal";
+import React, { Dispatch, SetStateAction, useState } from "react";
+import AddDataModal from "../../components/Modal/AddDataModal";
+import Map from "../../components/Map/Map";
 
 type CreateEventModalType = {
   setActive: Dispatch<SetStateAction<boolean>>;
 };
 export default function CreateEventModal({ setActive }: CreateEventModalType) {
+  const [postcode, setPostcode] = useState<string>("10000");
+  const [townName, setTownName] = useState<string>("Zagreb");
+  const [streetName, setStreetName] = useState<string>("");
+  const [location, setLocation] = useState<{
+    lat: number;
+    lng: number;
+  }>({
+    lat: 45.815399,
+    lng: 15.966568,
+  });
+
   return (
-    <Modal title={"Create event"} modalChange={() => setActive(false)}>
-      <p>Modal</p>
-    </Modal>
+    <AddDataModal title={"Create event"} modalChange={() => setActive(false)}>
+      <div className="flex-col w-full flex items-center">
+        <Map
+          setPostcode={setPostcode}
+          setTownName={setTownName}
+          setStreetName={setStreetName}
+          location={location}
+          setLocation={setLocation}
+        />
+      </div>
+    </AddDataModal>
   );
 }
